bf605c967430b6a42dc25bce35e78b174f32c48e
Dean DeHart <dddehart@oakland.edu>
2016-10-12 00:19:11 -0400

    Day Night cycle and dynamic lighting setup.

--- Assets/Scripts/DayNight/DayNightCycle.cs
+++ Assets/Scripts/DayNight/DayNightCycle.cs
@@ -15,8 +15,11 @@ public class DayNightCycle : MonoBehaviour {
 	public float intensity;
 	public Color fogDay = Color.grey;
 	public Color fogNight = Color.black;
-
 	public int speed;
+
+	private Int32 secondsInDay = 86400;
+	private UInt16 secondsInHalfDay = 43200;
+	private Int16 secondsInQuarterDay = 21600;
 
 	
 	// Update is called once per frame
@@ -26,9 +29,19 @@ public class DayNightCycle : MonoBehaviour {
 
 	public void changeTime() {
 		time += Time.deltaTime * speed;
-		if (time > 864000) {
+		if (time > secondsInDay) {
 			days += 1;
 			time = 0;
 		}
+		currentTime = TimeSpan.FromSeconds (time);
+		string[] tempTime = currentTime.ToString ().Split (":"[0]);
+		timeText.text = tempTime [0] + ":" + tempTime [1];
+
+		sunTransform.rotation = Quaternion.Euler (new Vector3((time - secondsInQuarterDay)/secondsInDay * 360, 0, 0));
+		intensity = (time < secondsInHalfDay) ? 1 - (secondsInHalfDay - time)/secondsInHalfDay : 1 - (secondsInHalfDay - time)/secondsInHalfDay * -1;
+		RenderSettings.fogColor = Color.Lerp (fogNight, fogDay, Mathf.Pow(intensity, 2));
+
+		Sun.intensity = intensity;
+
 	}
 }
---
+++ Assets/Scripts/DayNight/SkyController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyController : MonoBehaviour {
+	Material sky;
+	//public Renderer water;
+
+	public Transform stars;
+	public Transform worldProbe;
+
+	// Use this for initialization
+	void Start () {
+		sky = RenderSettings.skybox;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		stars.transform.rotation = transform.rotation;
+		Vector3 tvec = Camera.main.transform.position;
+		worldProbe.transform.position = tvec;
+
+
+	}
+}
