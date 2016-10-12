177d7435df8fd58f2a338c3e51f0cf6210671543
Dean DeHart <dddehart@oakland.edu>
2016-10-11 20:17:38 -0400

    Fixed gitignore, should be properly ignoring files now.

---
+++ Assets/Scripts/DayNight/DayNightCycle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class DayNightCycle : MonoBehaviour {
+
+	public float time;
+	public TimeSpan currentTime;
+	public Transform sunTransform;
+	public Light Sun;
+	public Text timeText;
+	public int days;
+
+	public float intensity;
+	public Color fogDay = Color.grey;
+	public Color fogNight = Color.black;
+
+	public int speed;
+
+	
+	// Update is called once per frame
+	void Update () {
+		changeTime ();
+	}
+
+	public void changeTime() {
+		time += Time.deltaTime * speed;
+		if (time > 864000) {
+			days += 1;
+			time = 0;
+		}
+	}
+}
