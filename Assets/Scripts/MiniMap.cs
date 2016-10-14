a942917a2eea312a8668aadf86535bd2bb1ef5c7
Dean DeHart <dddehart@oakland.edu>
2016-10-14 15:52:52 -0400

    Minimap now uses a script. This locks minimap rotation.

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+	public Transform target;
+	public float distance;
+	private Vector3 targetLocale;
+	// Use this for initialization
+	void Start () {
+		targetLocale = new Vector3(target.position.x, target.position.y + distance, target.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		targetLocale.z = target.position.z;
+		targetLocale.x = target.position.x;
+		transform.position = targetLocale;
+	}
+}
--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -9,6 +9,7 @@ public class Flashlight : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		light = GetComponent<Light> ();
+		light.enabled = false;
 	}
 	
 	// Update is called once per frame
