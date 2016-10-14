cff3705e8ef2c4e1c81e5284a01d765d1db28f5e
Dean DeHart <dddehart@oakland.edu>
2016-10-14 15:16:53 -0400

    Added flashlight, updated Reflection probe.

---
+++ Assets/Scripts/Player/Flashlight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+	public AudioClip soundOn;
+	public AudioClip soundOff;
+	private Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse1)) {
+			if (light.enabled == false) {
+				light.enabled = true;
+				AudioSource.PlayClipAtPoint (soundOn, transform.position,0.1f);
+			} else {
+				light.enabled = false;
+				AudioSource.PlayClipAtPoint (soundOff, transform.position,0.1f);
+			}
+		}
+	}
+}
